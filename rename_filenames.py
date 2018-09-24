f2f0aef3771d52da5ef836b6812a4f7a2a1c9e6b
leej3 <johnleenimh@gmail.com>
2018-09-24 13:22:44 -0400

    update files in repo

--- rename_filenames.py
+++ rename_filenames.py
@@ -1,8 +1,14 @@
 # coding: utf-8
 from pathlib import Path
-for f in Path('nifti_dir').glob('*'):
+import sys
+nifti_dir = sys.argv[1]
+suffixes = ['.nii','.json','.bval','.bvec']
+if not nifti_dir:
+	nifti_dir = 'dcm2niix_head'
+
+for f in [x for x in Path(nifti_dir).glob('*') if x.suffix in suffixes]:
     filenum = f.stem.split('_')[-1]
     fname = '{:04d}'.format(int(filenum))  + '_' + f.name.replace('(','_').replace(')','_').replace('=','_')
-    dest  = Path('nifti_dir') /  fname 
+    dest  = Path(nifti_dir) /  fname 
     print(dest)
     f.rename(dest)
